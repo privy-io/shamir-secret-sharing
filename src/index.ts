3333451987485785aa500b07e73f4b2972ff68b2
Andrew MacPherson <github@andrewmohawk.com>
2025-01-10 17:47:42 -0500

    Revert '8d6d39d: Merge pull request #2 from privy-io/PVY-01-002' (#22)
    * Revert '8d6d39d: Merge pull request #2 from privy-io/PVY-01-002'

--- src/index.ts
+++ src/index.ts
@@ -144,33 +144,11 @@ function evaluate(coefficients: Uint8Array, x: number, degree: number) {
   return result;
 }
 
-function getRandomByte(): number {
-  return getRandomBytes(1)[0]!;
-}
-
-function getNonZeroRandomByte(): number {
-  while (true) {
-    const byte = getRandomByte();
-    if (byte > 0) {
-      return byte;
-    }
-  }
-}
-
 // Creates a pseudo-random set of coefficients for a polynomial.
 function newCoefficients(intercept: number, degree: number): Readonly<Uint8Array> {
   const coefficients = new Uint8Array(degree + 1);
-
-  // The first byte is always the intercept
   coefficients[0] = intercept;
-
-  for (let i = 1; i <= degree; i++) {
-    // degree is equal to t-1, where t is the threshold of required shares.
-    // The coefficient at t-1 cannot equal 0.
-    const coefficientTMinus1 = i === degree;
-    coefficients[i] = coefficientTMinus1 ? getNonZeroRandomByte() : getRandomByte();
-  }
-
+  coefficients.set(getRandomBytes(degree), 1);
   return coefficients;
 }
 
